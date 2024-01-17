5b6dbf681eb5ca20e5d4e594b37c9c6ca9813247
Lucas Alvares <lucas.alvaress@hotmail.com>
2024-01-17 04:56:47 -0300

    🚚 rename some routes and create category routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,15 +3,26 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
 
 const router = Router();
 
 // USER ROUTES
 
-router.post("/users", new CreateUserController().handle);
+router.post("/register", new CreateUserController().handle);
 
 router.post("/login", new AuthUserController().handle);
 
 router.get("/user", isAuthenticated, new DetailUserController().handle);
+
+// CATEGORY ROUTES
+router.post(
+    "/registerCategory",
+    isAuthenticated,
+    new CreateCategoryController().handle
+);
+
+router.get("/category", isAuthenticated, new ListCategoryController().handle);
 
 export { router };
