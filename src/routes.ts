c2cedaaa5499d6d8169205ee2493d916ee28b4c3
Lucas Alvares <lucas.alvaress@hotmail.com>
2024-01-17 05:43:39 -0300

    ✨ create products routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,20 @@
 import { Router } from "express";
+import multer from "multer";
+
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
+
+import uploadConfig from "./config/multer";
+import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
 
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"));
 
 // USER ROUTES
 
@@ -24,5 +32,19 @@ router.post(
 );
 
 router.get("/category", isAuthenticated, new ListCategoryController().handle);
+
+// PRODUCTS ROUTES
+router.post(
+    "/registerProduct",
+    isAuthenticated,
+    upload.single("file"),
+    new CreateProductController().handle
+);
+
+router.get(
+    "/category/products",
+    isAuthenticated,
+    new ListByCategoryController().handle
+);
 
 export { router };
